6c815599ac6a6ee95713a29d60024c4d4a2a98db
c0a241717f <c0a241717f@edu.teu.ac.jp>
2024-06-14 18:01:45 +0900

    aaa

--- live_share.py
+++ live_share.py
@@ -5,5 +5,7 @@ student1_last3 = int(student1[-3:])
 student2_last3 = int(student2[-3:])
 
 result = student1_last3 + student2_last3
+result2 = student1_last3 * student2_last3
 
 print("変数1と変数2を足すと",result,"になります。")
+print("変数1と変数2をかけると",result2,"になります")
