060967cd180436a70ac7a7f859117171b0ad6cb7
c0a241717f <c0a241717f@edu.teu.ac.jp>
2024-06-14 17:43:51 +0900

    first commit

---
+++ live_share.py
@@ -0,0 +1,9 @@
+student1 = "C0A24027"
+student2 = "C0A24171"
+
+student1_last3 = int(student1[-3:])
+student2_last3 = int(student2[-3:])
+
+result = student1_last3 + student2_last3
+
+print("変数1と変数2を足すと",result,"になります。")
